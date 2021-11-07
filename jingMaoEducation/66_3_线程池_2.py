7415f890f2f80df98558f69463d24464e8aca163
muyun <1219258598@qq.com>
2021-11-07 22:36:34 +0800

    改名and加注释

--- jingMaoEducation/66_3_线程池_2.py
+++ jingMaoEducation/66_3_线程池_2.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     with ThreadPoolExecutor(max_workers=5) as t:
         tasks = [t.submit(spider, i) for i in range(1, 5)]
 
+        # as_completed():每当有一个线程完成任务，会自动用t.result()获取返回结果
         for t in as_completed(tasks):
             data = t.result()
             print(data)
