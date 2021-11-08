e6bb16719fb44520460440dfbe4c6acce997c4cd
muyun <1219258598@qq.com>
2021-11-08 11:09:11 +0800

    添加push_plus官网信息

--- jingMaoEducation/72_消息推送push_plus.py
+++ jingMaoEducation/72_消息推送push_plus.py
@@ -1,3 +1,7 @@
+"""
+使用push_plus进行微信消息推送
+官网：http://pushplus.hxtrip.com/message
+"""
 import requests
 
 url = "http://pushplus.hxtrip.com/send"
@@ -5,7 +9,7 @@ url = "http://pushplus.hxtrip.com/send"
 title = "测试"
 content = "我的兼职猫程序搞定了！！！！！！！！"
 params = {
-    "token": "自己的token",
+    "token": "e71cb823d3564e939a79a06ffc6e9114",
     "title": title,
     "content": content,
 }
