fc4d43c711594a658d30c24ab80d2ca69c308c8a
muyun <1219258598@qq.com>
2021-11-22 11:36:51 +0800

    redis的使用

--- jingMaoEducation/30_数据库_redis_2.py
+++ jingMaoEducation/30_数据库_redis_2.py
@@ -1,41 +1,33 @@
-"""
-redis数据库：非关系型数据库
-"""
-
 from redis import StrictRedis
 
-r = StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
+"""
+redis: 非关系型数据库
+"""
+r = StrictRedis(host="localhost", port=6379, db=2, decode_responses=True)
 
 
 def redis_test():
-    # 单条数据
-    r.set("age", 19)  # 添加数据
-    age = r.get("age")  # 获取数据
-    print(age)
+    # 添加数据
+    # r.set("name", "zhangsan")  # 一次性插入一条数据
 
-    # 多条数据
-    r.mset({"name": "zhangsan", "age": 19})
-    print(r.mget("name", "age"))
+    # r.mset({"name": "张三", "age": 19, "score": 90})  # 一次性插入多条数据
+    # name = r.get("name")  # # 一次性获取一条数据
+    # print(name)
 
-    # job_info = {
-    #     "job_id": "0006dfe42a764216921b402d401f404f31aee685",
-    #     "job_title": "日结丨文字聊天员丨陪伴赚钱",
-    #     "job_url": "http://xinyu.jianzhimao.com/job/V3hNODAzUDl4MEE9.html",
-    #     "visited": 813,
-    # }
-    # r.set("0006dfe42a764216921b402d401f404f31aee685",f"{job_info}")
-    # print(r.get("0006dfe42a764216921b402d401f404f31aee685"))
+    # print(r.mget("name", "age", "score"))  # 一次性获取多条数据
 
-    # 删除数据
-    r.delete("0006dfe42a764216921b402d401f404f31aee685")
+    # r.delete("name")  # 删除数据
 
+    # is_exist = r.exists("name")  # 判断数据是否存在，存在返回1，否则返回0
+    # print(is_exist)
 
-    # 判断是否存在
-    score = r.exists("score")  # 如果有就返回1，否则返回0
-    print(score)
+    json_data = {
+        "name": "zhangsan",
+        "age": 19,
+        "score": 90
+    }
 
-    if r.exists("score"):
-        print(r.get("score"))
+    r.set("student_info", f"{json_data}")
 
 
 if __name__ == '__main__':
